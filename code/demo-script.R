cba56805f7bad4805ecbdde38df289d36a2d37cf
Brenton Law <brentonlaw@berkeley.edu>
2019-10-03 16:54:51 -0700

    Add data, R

---
+++ code/demo-script.R
@@ -0,0 +1,7 @@
+# Title: Demo script in R
+# Description: We are using a script as a demo in Stat 133
+# Input: We are taking inputs of data tables
+# Output: We are sending out a summary
+
+library(dplyr)
+library(ggplot2)
